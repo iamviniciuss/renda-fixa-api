f766e2c90346653c07170757b095f5d0c80daaa1
vinicrewhu <viniciuss@crewhu.com>
2022-07-05 20:31:04 -0300

    health check router.

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	infra "github.com/Vinicius-Santos-da-Silva/renda-fixa-api/src/infra/http"
+	health "github.com/Vinicius-Santos-da-Silva/renda-fixa-api/src/infra/web/health"
 	profit "github.com/Vinicius-Santos-da-Silva/renda-fixa-api/src/infra/web/profit"
 )
 
@@ -9,8 +10,9 @@ func main() {
 
 	http := infra.NewFiberHttp()
 	profit.ProfitRouter(http)
+	health.HealthRouter(http)
 
-	err := http.ListenAndServe(":9002")
+	err := http.ListenAndServe(":80")
 
 	if err != nil {
 		panic(err)
