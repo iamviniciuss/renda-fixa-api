66a360cc1f1a3685fb8b25a6ff7ec36938b3b348
vinicrewhu <viniciuss@crewhu.com>
2022-07-05 20:31:04 -0300

    health check router.

---
+++ src/infra/web/health/controller/Health.go
@@ -0,0 +1,22 @@
+package controller
+
+import (
+	"time"
+
+	http "github.com/Vinicius-Santos-da-Silva/renda-fixa-api/src/infra/http"
+)
+
+type HealthCtrl struct {
+	Now time.Time
+}
+
+func NewHealthCtrl(now time.Time) *HealthCtrl {
+	HealthCtrl := new(HealthCtrl)
+	HealthCtrl.Now = now
+
+	return HealthCtrl
+}
+
+func (gs *HealthCtrl) Index(params map[string]string, body []byte, queryArgs http.QueryParams) (interface{}, error) {
+	return "OK", nil
+}
